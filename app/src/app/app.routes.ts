a98a2702121c40357c7baef2fab1a21b254d63f8
Ben Sivoravong <bensivo@gmail.com>
2025-07-04 12:27:59 -0500

    Basic notes ui

---
+++ app/src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Homepage } from './pages/homepage/homepage';
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    title: 'Home',
+    path: '',
+    component: Homepage,
+  }
+];
