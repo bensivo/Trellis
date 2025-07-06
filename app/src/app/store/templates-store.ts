14330b2bf29b90dfe294775125b28b5f19d45735
Ben Sivoravong <bensivo@gmail.com>
2025-07-06 15:18:49 -0500

    updated todo items

--- app/src/app/store/templates-store.ts
+++ app/src/app/store/templates-store.ts
@@ -17,48 +17,6 @@ const initialState: TemplatesStore = {
         ],
         content: null
     },
-    {
-        id: 2,
-        name: "Project",
-        fields: [
-            { name: "Start Date", type: TemplateFieldType.DATE },
-            { name: "End Date", type: TemplateFieldType.DATE },
-            { name: "Status", type: TemplateFieldType.SELECT },
-            { name: "Priority", type: TemplateFieldType.SELECT }
-        ],
-        content: null
-    },
-    {
-        id: 3,
-        name: "Person",
-        fields: [
-            { name: "Title", type: TemplateFieldType.TEXT },
-            { name: "Department", type: TemplateFieldType.TEXT },
-            { name: "Manager", type: TemplateFieldType.NUMBER },
-            { name: "Email", type: TemplateFieldType.TEXT },
-        ],
-        content: null
-
-    },
-    {
-        id: 4,
-        name: "Segment",
-        fields: [
-            { name: "Created Date", type: TemplateFieldType.DATE },
-        ],
-        content: null
-
-    },
-    {
-        id: 5,
-        name: "Memo",
-        fields: [
-            { name: "Subject", type: TemplateFieldType.TEXT },
-            { name: "Date", type: TemplateFieldType.DATE },
-        ],
-        content: null
-
-    }
     ]
 }
 
